02b6f6bb68eb657e68ef393156bb14d158ab5182
ivaniv_maks <ivanivmaks0@gmail.com>
2023-05-31 15:34:44 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,28 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+const fetchUserFromServer = username => {
+  return new Promise((resolve, reject) => {
+    console.log(`Fetching data for ${username}`);
+
+    setTimeout(() => {
+      const isSuccess = true;
+
+      if (isSuccess) {
+        resolve('success value');
+      } else {
+        reject('error');
+      }
+    }, 2000);
+  });
+};
+
+fetchUserFromServer('Mango')
+  .then(user => console.log(user))
+  .catch(error => console.error(error));
