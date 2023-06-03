c986a22ea52ec730507a55e9f230ee16bf9f382d
ivanivmaks <122301032+ivanivmaks@users.noreply.github.com>
2023-06-03 12:04:49 +0000

    Deploying to gh-pages from @ ivanivmaks/goit-js-hw-09@dab8007e33cb38897fd990b8779aec31ad8931b3 🚀

--- 03-promises.b0bda94a.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;(n="Mango",new Promise((function(o,c){console.log("Fetching data for ".concat(n)),setTimeout((function(){o("success value")}),2e3)}))).then((function(n){return console.log(n)})).catch((function(n){return console.error(n)}))}();
-//# sourceMappingURL=03-promises.b0bda94a.js.map
