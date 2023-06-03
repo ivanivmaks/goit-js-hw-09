dab8007e33cb38897fd990b8779aec31ad8931b3
ivaniv_maks <ivanivmaks0@gmail.com>
2023-06-03 15:04:12 +0300

    updated

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,28 +1,41 @@
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+import Notiflix from 'notiflix';
 
-const fetchUserFromServer = username => {
+const form = document.querySelector('form');
+const delay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+form.addEventListener('submit', submit);
+
+function submit(event) {
+  event.preventDefault();
+  let wait = Number(delay.value);
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, wait)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    wait += Number(step.value);
+  }
+  event.currentTarget.reset();
+}
+
+function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    console.log(`Fetching data for ${username}`);
-
+    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
-      const isSuccess = true;
-
-      if (isSuccess) {
-        resolve('success value');
+      if (shouldResolve) {
+        resolve({ position, delay });
       } else {
-        reject('error');
+        reject({ position, delay });
       }
-    }, 2000);
+    }, delay);
   });
-};
-
-fetchUserFromServer('Mango')
-  .then(user => console.log(user))
-  .catch(error => console.error(error));
+}
