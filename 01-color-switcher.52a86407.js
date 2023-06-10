8dd6218f4c5465f6cdb72c607ff1e988826ede7a
ivanivmaks <122301032+ivanivmaks@users.noreply.github.com>
2023-06-10 13:55:49 +0000

    Deploying to gh-pages from @ ivanivmaks/goit-js-hw-09@3ebe06f5104496fffa1c5a43b84df60e8675bd05 🚀

---
+++ 01-color-switcher.52a86407.js
@@ -0,0 +1,2 @@
+const t=document.querySelector(".start-btn"),e=document.querySelector(".stop-btn");let d=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.52a86407.js.map
---
+++ 01-color-switcher.c5b22261.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(".start-btn"),e=document.querySelector(".stop-btn"),n=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.c5b22261.js.map
